a487a6ed728796099e1e0f7f15341525a42e8356
UnrealSec <arran.bishop89@aol.com>
2020-03-01 21:03:22 +0200

    Added Line, Line2D structures and JSdoc

--- geometry.js
+++ geometry.js
@@ -3,6 +3,11 @@
 class Size {
     width = 0; height = 0;
 
+    /**
+    * Create new Size
+    * @param {number} width
+    * @param {number} height
+    */
     constructor(width=0, height=0) {
         this.width = width;
         this.height = height;
@@ -17,6 +22,11 @@ class Size {
 class Point {
     x = 0; y = 0;
 
+    /**
+    * Create new Point
+    * @param {number} x
+    * @param {number} y
+    */
     constructor(x=0, y=0) {
         this.x = x;
         this.y = y;
@@ -25,6 +35,10 @@ class Point {
     get X() { return this.x; } set X(x) { this.x = x; }
     get Y() { return this.y; } set Y(y) { this.y = y; }
 
+    /**
+    * Get distance between this Point and another Point
+    * @param {Point} point
+    */
     distanceTo(point) {
         var a = this;
         var b = point;
@@ -34,12 +48,20 @@ class Point {
         return distance;
     }
 
+    /**
+    * Get angle between this Point and another Point in radians and degrees
+    * @param {Point} point
+    */
     angleTo(point) {
         var r = Math.atan2(point.Y - this.Y, point.X - this.X);
         var d = r * (180/Math.PI);
         return {radians: r, degrees: d};
     }
 
+    /**
+    * Test if this Point is inside a Rectangle
+    * @param {Rectangle} rect
+    */
     isInside(rect) {
         var a = this;
         var b = rect;
@@ -58,6 +80,11 @@ class Point {
 class Rectangle {
     point = null; size = null;
 
+    /**
+    * Create new Rectangle
+    * @param {Point} point
+    * @param {Size} size
+    */
     constructor(point, size) {
         this.point = point;
         this.size = size;
@@ -70,6 +97,10 @@ class Rectangle {
     get Point() { return this.point; } set Point(point) { this.point = point; }
     get Size() { return this.size; } set Size(size) { this.size = size; }
 
+    /**
+    * Test if this Rectangle intersects with another Rectangle
+    * @param {Rectangle} rect
+    */
     intersectsWith(rect) {
         var a = this;
         var b = rect;
@@ -84,6 +115,10 @@ class Rectangle {
         return false;
     }
 
+    /**
+    * Test if this Rectangle is inside another Rectangle
+    * @param {Rectangle} rect
+    */
     isInside(rect) {
         var a = rect;
         var b = this;
@@ -100,6 +135,11 @@ class Rectangle {
 class Circle {
     point = null; radius = 0;
 
+    /**
+    * Create new Circle
+    * @param {Point} point
+    * @param {number} radius
+    */
     constructor(point, radius=0) {
         this.point = point;
         this.radius = radius;
@@ -110,6 +150,10 @@ class Circle {
     get Point() { return this.point; } set Point(point) { this.point = point; }
     get Radius() { return this.radius; } set Radius(r) { this.radius = r; }
 
+    /**
+    * Get distance from this Circle's center point to another Circle's center point
+    * @param {Circle} circle
+    */
     distanceTo(circle) {
         var a = this;
         var b = circle;
@@ -119,6 +163,10 @@ class Circle {
         return distance;
     }
 
+    /**
+    * Test if this Circle is inside another Circle
+    * @param {Circle} circle
+    */
     isInside(circle) {
         var distance = this.distanceTo(circle);
         if (distance+this.Radius <= circle.Radius) {
@@ -127,6 +175,10 @@ class Circle {
         return false;
     }
 
+    /**
+    * Test if this Circle intersects with another Circle
+    * @param {Circle} circle
+    */
     intersectsWith(circle) {
         var distance = this.distanceTo(circle);
         var radius_sum = this.Radius+circle.Radius;
@@ -142,6 +194,12 @@ class Circle {
 class Size3D {
     width = 0; height = 0; depth = 0;
 
+    /**
+    * Create new Size3D
+    * @param {number} width
+    * @param {number} height
+    * @param {number} depth
+    */
     constructor(width=0, height=0, depth=0) {
         this.width = width;
         this.height = height;
@@ -158,6 +216,12 @@ class Size3D {
 class Point3D {
     x = 0; y = 0; z = 0;
 
+    /**
+    * Create new Point3D
+    * @param {number} x
+    * @param {number} y
+    * @param {number} z
+    */
     constructor(x=0, y=0, z=0) {
         this.x = x;
         this.y = y;
@@ -168,6 +232,10 @@ class Point3D {
     get Y() { return this.y; } set Y(y) { this.y = y; }
     get Z() { return this.z; } set Z(z) { this.z = z; }
 
+    /**
+    * Get distance between this Point3D and another Point3D
+    * @param {Point3D} point
+    */
     distanceTo(point) {
         var a = this;
         var b = point;
@@ -177,6 +245,10 @@ class Point3D {
         return distance;
     }
 
+    /**
+    * Test if this Point3D is inside a Cube
+    * @param {Cube} cube
+    */
     isInside(cube) {
         var a = this;
         var b = cube;
@@ -196,9 +268,14 @@ class Point3D {
 class Cube {
     point = null; size = null;
 
-    constructor(point3D, size3D) {
-        this.point = point3D;
-        this.size = size3D;
+    /**
+    * Create new Cube
+    * @param {Point3D} point
+    * @param {Size3D} size
+    */
+    constructor(point, size) {
+        this.point = point;
+        this.size = size;
     }
 
     get X() { return this.point.X; } set X(x) { this.point.X = x; }
@@ -210,6 +287,10 @@ class Cube {
     get Point3D() { return this.point; } set Point3D(point) { this.point = point; }
     get Size3D() { return this.size; } set Size3D(size) { this.size = size; }
 
+    /**
+    * Test if this Cube intersects with another Cube
+    * @param {Cube} cube
+    */
     intersectsWith(cube) {
         var a = this;
         var b = cube;
@@ -226,6 +307,10 @@ class Cube {
         return false;
     }
 
+    /**
+    * Test if this Cube is inside another Cube
+    * @param {Cube} cube
+    */
     isInside(cube) {
         var a = this;
         var b = cube;
@@ -243,6 +328,11 @@ class Cube {
 class Sphere {
     point = null; radius = 0;
 
+    /**
+    * Create new Sphere
+    * @param {Point} point
+    * @param {number} radius
+    */
     constructor(point, radius=0) {
         this.point = point;
         this.radius = radius;
@@ -254,6 +344,10 @@ class Sphere {
     get Point3D() { return this.point; } set Point3D(point) { this.point = point; }
     get Radius() { return this.radius; } set Radius(r) { this.radius = r; }
 
+    /**
+    * Get distance from this Sphere's center point to another Sphere's center point
+    * @param {Sphere} sphere
+    */
     distanceTo(sphere) {
         var a = this;
         var b = sphere;
@@ -263,6 +357,10 @@ class Sphere {
         return distance;
     }
 
+    /**
+    * Test if this Sphere is inside another Sphere
+    * @param {Sphere} sphere
+    */
     isInside(sphere) {
         var distance = this.distanceTo(sphere);
         if (distance+this.Radius <= sphere.Radius) {
@@ -271,6 +369,10 @@ class Sphere {
         return false;
     }
 
+    /**
+    * Test if this Sphere intersects with another Sphere
+    * @param {Sphere} sphere
+    */
     intersectsWith(sphere) {
         var distance = this.distanceTo(sphere);
         var radius_sum = this.Radius+sphere.Radius;
@@ -283,4 +385,61 @@ class Sphere {
     }
 }
 
-if (typeof(module)!='undefined') module.exports = { Size, Point, Rectangle, Circle, Point3D, Size3D, Cube, Sphere };
+class Line {
+    point_a = null;
+    point_b = null;
+
+    /**
+    * Create new Line
+    * @param {Point} a
+    * @param {Point} b
+    */
+    constructor(a, b) {
+        this.point_a = a;
+        this.point_b = b;
+    }
+
+    get A() { return this.point_a; } set A(point) { this.point_a = point; }
+    get B() { return this.point_b; } set B(point) { this.point_b = point; }
+
+    /**
+    * Get Line's length (distance between point A and B)
+    */
+    length() {
+        return this.point_a.distanceTo(this.point_b);
+    }
+
+    /**
+    * Get angle between Line's two points in radians and degrees
+    */
+    angle() {
+        return this.point_a.angleTo(point_b);
+    }
+}
+
+class Line3D {
+    point_a = null;
+    point_b = null;
+
+    /**
+    * Create new Line3D
+    * @param {Point3D} a
+    * @param {Point3D} b
+    */
+    constructor(a, b) {
+        this.point_a = a;
+        this.point_b = b;
+    }
+
+    get A() { return this.point_a; } set A(point) { this.point_a = point; }
+    get B() { return this.point_b; } set B(point) { this.point_b = point; }
+
+    /**
+    * Get Line's length (distance between point A and B)
+    */
+    length() {
+        return this.point_a.distanceTo(this.point_b);
+    }
+}
+
+if (typeof(module)!='undefined') module.exports = { Size, Point, Rectangle, Circle, Point3D, Size3D, Cube, Sphere, Line, Line3D };
